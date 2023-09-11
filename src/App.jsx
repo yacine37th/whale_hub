e9dfee5734ce3dc87c323cb42b151357bb093082
yacine37th <y.titouah@esi-sba.dz>
2023-09-11 22:41:36 +0100

    probleme of taillwind fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './home/Home'
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 
-import "./App.css";
-import Home from "./home/Home";
-import Login from "./auth/Login";
+import Login from './auth/Login'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Routes>
+     <Routes>
+      
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+       
       </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div><h1>Home</h1></div>
+    <div><h1 className='text-yellow-500 bg-red-500'>Home</h1></div>
   )
 }
 
