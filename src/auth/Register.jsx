f9191b56ea1c08a66ecf8598ba669dee4aa96ad8
yacine37th <y.titouah@esi-sba.dz>
2023-10-22 18:15:57 +0100

    radio input added to register page

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -34,6 +34,13 @@ function Register() {
   const [usdt, setusdt] = useState("");
   const [amountInvest, setamountInvest] = useState(0);
   const [loading, setloading] = useState(false);
+
+  const [gender, setgender] = useState();
+
+  const handlegender = (e) => {
+    setgender(e.target.value);
+  };
+
   const register = async (
     au,
     em,
@@ -382,21 +389,102 @@ function Register() {
               <p className="text-red-700 text-xs flex items-start"></p>
             </div>
 
+            <div className="flex flex-col px-10 my-5">
+              <p className="text-2xl">PLEASE SELECT THE PACK</p>
 
+              <div className="flex gap-3">
+                <input
+                  id="normal_pack"
+                  type="radio"
+                  name="normal"
+                  value="normal_pack"
+                  onChange={handlegender}
+                />
+                <label htmlFor="normal_pack" className="mt-px inline-block pl-[0.15rem] hover:cursor-pointer">
+                  NORMAL PACK
+                </label>
+              </div>
 
+              <div className="flex gap-3">
+                <input
+                  id="golden_pack"
+                  type="radio"
+                  name="normal"
+                  value="golden_pack"
+                  onChange={handlegender}
+                />
+                <label htmlFor="golden_pack" className="mt-px inline-block pl-[0.15rem] hover:cursor-pointer">
+                  GOLDEN PACK
+                </label>
+              </div>
+
+              <div className="flex gap-3">
+                <input
+                  className="checked:bg-blue-900 p-3 bg-red-700"
+                  id="diamand_pack"
+                  type="radio"
+                  name="normal"
+                  value="diamand_pack"
+                  onChange={handlegender}
+                />
+                <label htmlFor="diamand_pack" className="mt-px inline-block pl-[0.15rem] hover:cursor-pointer">
+                  DIAMAND PACK
+                </label>
+              </div>
+              {/* <input
+                type="radio"
+                name="gender"
+                value="Female"
+                onChange={handlegender}
+              />
+              Female */}
+            </div>
+
+            {/* <div className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
+              <input
+                className="relative float-left -ml-[1.5rem] mr-1 mt-0.5 h-5 w-5 appearance-none rounded-full border-2 border-solid border-neutral-300 before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:border-primary checked:after:bg-primary checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:border-primary checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:border-neutral-600 dark:checked:border-primary dark:checked:after:border-primary dark:checked:after:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:border-primary dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+                type="radio"
+                name="flexRadioDefault"
+                id="radioDefault01"
+              />
+              <label
+                className="mt-px inline-block pl-[0.15rem] hover:cursor-pointer"
+                htmlFor="radioDefault01"
+              >
+                Default radio
+              </label>
+            </div>
+            <div className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
+              <input
+                className="relative float-left -ml-[1.5rem] mr-1 mt-0.5 h-5 w-5 appearance-none rounded-full border-2 border-solid border-neutral-300 before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:border-primary checked:after:bg-primary checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:border-primary checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:border-neutral-600 dark:checked:border-primary dark:checked:after:border-primary dark:checked:after:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:border-primary dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+                type="radio"
+                name="flexRadioDefault"
+                id="radioDefault02"
+                checked
+              />
+              <label
+                className="mt-px inline-block pl-[0.15rem] hover:cursor-pointer"
+                htmlFor="radioDefault02"
+              >
+                Default checked radio
+              </label>
+            </div> */}
             <button
               onClick={() => {
-                register(
-                  auth,
-                  email,
-                  password,
-                  fullName,
-                  username,
-                  betcoinAccount,
-                  usdt,
-                  Baridymob
-                );
+                console.log(gender);
               }}
+              // onClick={() => {
+              //   register(
+              //     auth,
+              //     email,
+              //     password,
+              //     fullName,
+              //     username,
+              //     betcoinAccount,
+              //     usdt,
+              //     Baridymob
+              //   );
+              // }}
               type="submit"
               className="
                   w-full p-4 button-background-register border-white   text-white  text-base
@@ -414,7 +502,11 @@ function Register() {
                   {/* <div class=" flex justify-center items-center">
                     <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-900"></div>
                   </div> */}
-                  <LoadingSpinner width={"w-5"} height={"h-5"} text={"Loading ..."} />
+                  <LoadingSpinner
+                    width={"w-5"}
+                    height={"h-5"}
+                    text={"Loading ..."}
+                  />
                 </div>
               ) : (
                 <p>ENTER</p>
