b04477b3a2c6041c475fa1b86d9ecc6e4c752c20
yacine37th <y.titouah@esi-sba.dz>
2023-10-31 15:54:18 +0100

    logout fixxed in the navBarAuth

--- src/auth/NavBarAuth.jsx
+++ src/auth/NavBarAuth.jsx
@@ -8,8 +8,8 @@ import { auth } from "../firebase/firebase";
 import { onLogout } from "../home/functions";
 
 export default function NavBarAuth() {
-  const [isLogin, setisLogin] = useState(false  );
-  const navigate = useNavigate()
+  const [isLogin, setisLogin] = useState(false);
+  const navigate = useNavigate();
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -31,7 +31,6 @@ export default function NavBarAuth() {
     });
 
     console.log("///////////////");
-
   }, []);
 
   // const onLogout = (au) => {
@@ -49,9 +48,11 @@ export default function NavBarAuth() {
   // };
 
   return (
-    <div className="background  absolute top-0 left-0  px-44 flex justify-between items-center w-full
+    <div
+      className="background  absolute top-0 left-0  px-44 flex justify-between items-center w-full
     max-[600px]:p-5
-    ">
+    "
+    >
       <div className="">
         <Link to={"/"}>
           <img
@@ -63,62 +64,72 @@ export default function NavBarAuth() {
           />
         </Link>
       </div>
-    {isLogin ?
-    (<div>
-    <button
-    onClick={()=>{onLogout(auth)
-      navigate('/',{replace : true})
-    }}
-      className="flex items-center background justify-between p-7 bg-transparent outline-none
+      {isLogin ? (
+        <div>
+          <button
+            onClick={() => {
+              // onLogout(auth)
+              signOut(auth)
+                .then(() => {
+                  // Sign-out successful.
+                  setisLogin(false);
+                  navigate("/", { replace: true });
+                })
+                .catch((error) => {
+                  // An error happened.
+                });
+             
+            }}
+            className="flex items-center background justify-between p-7 bg-transparent outline-none
 focus:outline-none border-none header-button rounded-none  text-white hover:text-white
 "
-    >
-      <span className=" max-[600px]:text-xs">Logout</span>
-    </button>
-  </div>)
-    :  ( <div className="flex
-   
-   ">
-     <a
-        href="https://telegram.me/WHALEHUBDZ2023"
-        target="_blank"
-       className="flex items-center background justify-between p-4 bg-transparent outline-none
+          >
+            <span className=" max-[600px]:text-xs">Logout</span>
+          </button>
+        </div>
+      ) : (
+        <div className="flex">
+          <a
+            href="https://telegram.me/WHALEHUBDZ2023"
+            target="_blank"
+            className="flex items-center background justify-between p-4 bg-transparent outline-none
   focus:outline-none border-none header-button rounded-none    max-[600px]:hidden
   "
-     >
-       <img src={telegram} className="mr-2" width={25} alt="" />{" "}
-       <span className="text-white">Telegram</span>
-     </a>
-     {/* https://web.facebook.com/profile.php?id=61551873030825&_rdc=1&_rdr */}
-     <a
-         href="https://web.facebook.com/profile.php?id=61551873030825&_rdc=1&_rdr"
-         target="_blank"
-       className="flex items-center background justify-between p-4 bg-transparent outline-none
+          >
+            <img src={telegram} className="mr-2" width={25} alt="" />{" "}
+            <span className="text-white">Telegram</span>
+          </a>
+          {/* https://web.facebook.com/profile.php?id=61551873030825&_rdc=1&_rdr */}
+          <a
+            href="https://web.facebook.com/profile.php?id=61551873030825&_rdc=1&_rdr"
+            target="_blank"
+            className="flex items-center background justify-between p-4 bg-transparent outline-none
   focus:outline-none border-none header-button rounded-none    max-[600px]:hidden
   "
-     >
-       <img src={facebook} className="mr-2" width={25} alt="" />{" "}
-       <span className="text-white">Facebook</span>
-     </a>
-     <Link to={"/register"} className="text-white hover:text-white ">
-       <button
-         className="flex items-center background justify-between p-7 bg-transparent outline-none
+          >
+            <img src={facebook} className="mr-2" width={25} alt="" />{" "}
+            <span className="text-white">Facebook</span>
+          </a>
+          <Link to={"/register"} className="text-white hover:text-white ">
+            <button
+              className="flex items-center background justify-between p-7 bg-transparent outline-none
   focus:outline-none border-none header-button rounded-none
   "
-       >
-         Create Account
-       </button>
-     </Link>{" "}
-     <Link to={"/login"} className="text-white hover:text-white ">
-       <button
-         className="flex items-center background justify-between p-7 bg-transparent outline-none
+            >
+              Create Account
+            </button>
+          </Link>{" "}
+          <Link to={"/login"} className="text-white hover:text-white ">
+            <button
+              className="flex items-center background justify-between p-7 bg-transparent outline-none
   focus:outline-none border-none header-button rounded-none
   "
-       >
-         Login
-       </button>
-     </Link>
-   </div>)}
+            >
+              Login
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
